462a5a4c7772e1c9649a96fdc2e925ba8d35e5db
AustHadd <124482341+AustHadd@users.noreply.github.com>
2023-03-27 20:39:22 -0500

    added server.py to the github
    Added the server. It is very simple. All it does so far is connect to the client and test if it can send/receive data.
    Once we have the rasberry pi sensor, it might have to change how the data is parsed/stored after the client sends it

---
+++ server.py
@@ -0,0 +1,25 @@
+import socket
+
+def _server():
+	host = socket.gethostname()
+	port = 1234
+	
+	server_socket = socket.socket()
+	server_socket.bind((host, port))
+	
+	server_socket.listen(2)
+	connection, address = server_socket.accept()
+	
+	while True:
+		#receiving data from the client
+		data = connection.recv(1024).decode()
+		if not data:
+			#data is not being received
+			break	#end the loop
+			
+		#not necessary, just to test if the server and client are properly sending/receiving
+		data = 'received data' 
+		connection.send(data.encode()) #send data to the client
+		
+	connection.close() #data isn't being received anymore so close the connection
+			

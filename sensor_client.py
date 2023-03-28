fff5395c6e30e57ec27ab79ddbe0046e21de9851
AustHadd <124482341+AustHadd@users.noreply.github.com>
2023-03-27 20:45:25 -0500

    added some comments to client_sensor.py

--- sensor_client.py
+++ sensor_client.py
@@ -4,15 +4,19 @@ import threading
 HOST = '' #server ip address
 PORT = 1234
 
+#will need to add code to determine the parking availability from the camera
+
 def _client():
-	client_socket = socket.socket()
-	client_socket.connect((host, port))
+	client_socket = socket.socket() #initialize the socket
+	client_socket.connect((host, port)) #connect the client to the server
 	
 	data = 1 #will be the availability/occupied parking spot data
 	
-	client_socket.send(data.encode())
-	data = client_socket.recv(1024).decode()# the response from the server. not necessary just for testing
+	client_socket.send(data.encode()) #send the data from the client to the server
 	
-	client_socket.close()
+	#the response from the server. not necessary just for testing
+	data = client_socket.recv(1024).decode() 
+	
+	client_socket.close() #close the client's connection with the server
 		
 		

51c374b6bf72b7c2112a5fd1895a3f5e53127c83
AustHadd <124482341+AustHadd@users.noreply.github.com>
2023-04-30 19:18:17 -0500

    cleaned up
    Cleaned up the code and made it easy to import and use in the main.py

--- ParkingSpotCreator.py
+++ ParkingSpotCreator.py
@@ -1,7 +1,9 @@
 import cv2
-import pickle
+import dill
 import numpy as np
 import cvzone
+
+posList = []
 
 
 class ParkingSpaces:
@@ -19,9 +21,8 @@ class ParkingSpaces:
         else:
             return False
 
-    def check_parking_space(self, processed_image):
+    def check_parking_space(self, processed_image, base_img):
         # cv2.rectangle(img, tuple(self.positionList[0]), tuple(self.positionList[1]), (255, 0, 255), 2)
-
         x1, y1 = self.positionList[0]
         x2, y2 = self.positionList[1]
 
@@ -33,35 +34,27 @@ class ParkingSpaces:
 
         img_crop = processed_image[min_y:max_y, min_x:max_x]
 
-        #cv2.imshow(str(self.positionList[0]), img_crop)
-        count = cv2.countNonZero(img_crop)
-        cvzone.putTextRect(img, str(count), (min_x, min_y+20), scale=1.5, thickness=2, offset=0)
+        # cv2.imshow(str(self.positionList[0]), img_crop)
+        pixel_count = cv2.countNonZero(img_crop)
+        cvzone.putTextRect(base_img, str(pixel_count), (min_x, min_y+20), scale=1.5, thickness=2, offset=0)
 
-        if count < 800:
-            color = (0, 255, 0)
-            thickness = 5
+        if pixel_count < 800:
             available = True
         else:
-            color = (0, 0, 255)
-            thickness = 2
             available = False
 
-        cv2.rectangle(img, tuple(self.positionList[0]), tuple(self.positionList[1]), color, thickness)
         return available
 
 
 # loads the parking spaces that have been created from file
 try:
     with open('CarSpacePos.pkl', 'rb') as f:
-        spacesList = pickle.load(f)
+        spacesList = dill.load(f)
 except FileNotFoundError:
     # if there is no pre-existing file with the spacesList to load from make blank one
     spacesList = []
-except pickle.UnpicklingError:
+except dill.UnpicklingError:
     print('Error Loading pickle data')
-
-
-posList = []
 
 
 def mouse_click(events, x, y, flags, params):
@@ -88,40 +81,52 @@ def mouse_click(events, x, y, flags, params):
 
     # logs the parking spaces that have been created into a file
     with open('CarSpacePos.pkl', 'wb') as f:
-        pickle.dump(spacesList, f)
+        dill.dump(spacesList, f)
 
 
-# this is called every "frame"
-# it uses opencv to read in the given image or video
-while True:
-    img = cv2.imread('CarParkImg.jpg')
+def create_parking_spots():
+    cap = cv2.VideoCapture('parkinglot.mp4')
+    success, img = cap.read()
 
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img_blur = cv2.GaussianBlur(img_gray, (3, 3), 1)
-    img_thresh = cv2.adaptiveThreshold(img_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                       cv2.THRESH_BINARY_INV, 25, 16)
+    cv2.imwrite("frame_0.jpg", img)
 
-    # manipulate the image to make it easier to detect empty spaces
-    img_med = cv2.medianBlur(img_thresh, 5)
-    kernel = np.ones((3,3), np.int8)
-    img_dil = cv2.dilate(img_med, kernel, iterations=1)
+    # this is called every "frame"
+    # it uses opencv to read in the given image or video
+    while True:
+        img = cv2.imread('frame_0.jpg')
 
-    for space in spacesList:
-        cv2.rectangle(img, tuple(space.positionList[0]), tuple(space.positionList[1]), (255, 0, 255), 2)
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img_blur = cv2.GaussianBlur(img_gray, (3, 3), 1)
+        img_thresh = cv2.adaptiveThreshold(img_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                           cv2.THRESH_BINARY_INV, 25, 16)
 
-    count = 0
+        # manipulate the image to make it easier to detect empty spaces
+        img_med = cv2.medianBlur(img_thresh, 5)
+        kernel = np.ones((3,3), np.int8)
+        img_dil = cv2.dilate(img_med, kernel, iterations=1)
 
-    # checks each parking space for car
-    for i in spacesList:
-        if i.check_parking_space(img_dil):
-            count += 1
+        for space in spacesList:
+            cv2.rectangle(img, tuple(space.positionList[0]), tuple(space.positionList[1]), (255, 0, 255), 2)
 
-    cvzone.putTextRect(img, str(count)+"/"+str(len(spacesList))+" available spots", (0, 20), scale=1.5, thickness=2, offset=0)
+        count = 0
 
-    # displays the image
-    cv2.imshow("Image", img)
-    #cv2.imshow("ImageBlur", img_blur)
-    #cv2.imshow("ImageThresh", img_dil)
+        # checks each parking space for car
+        for i in spacesList:
+            available = i.check_parking_space(img_dil, img)
+            if available:
+                color = (0, 255, 0)
+                thickness = 5
+                count += 1
+            else:
+                color = (0, 0, 255)
+                thickness = 2
 
-    cv2.setMouseCallback("Image", mouse_click)
-    cv2.waitKey(1)
+            cv2.rectangle(img, tuple(i.positionList[0]), tuple(i.positionList[1]), color, thickness)
+
+        cvzone.putTextRect(img, str(count)+"/"+str(len(spacesList))+" available spots", (0, 20), scale=1.5, thickness=2, offset=0)
+
+        # displays the image
+        cv2.imshow("Image", img)
+
+        cv2.setMouseCallback("Image", mouse_click, posList)
+        cv2.waitKey(1)

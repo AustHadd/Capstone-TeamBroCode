931404e0f71949abbf2ba9201ce0c8d0453a5599
HO4678 <123117953+HO4678@users.noreply.github.com>
2023-11-02 22:54:44 -0500

    Delete app directory

--- app/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('parking/', include('parking.urls')),
-    path('search/', views.search_parking, name='search_parking'),
-]
-

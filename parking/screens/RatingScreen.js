0388bcdac4809897d9ecb58084a97993dead118f
Nathan <97767955+nathanntk@users.noreply.github.com>
2023-11-05 09:29:36 -0600

    Update RatingScreen.js

--- parking/screens/RatingScreen.js
+++ parking/screens/RatingScreen.js
@@ -1,44 +1,63 @@
-  import React, { Component } from 'react';
-import { View, Text, StyleSheet, ImageBackground, Image} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ImageBackground, Image, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import UNT1 from '../assets/UNTbackground1.png'
-export default class InitialPage extends Component {
-  render() {
+import UNT1 from '../assets/UNTbackground1.png';
+
+export default function Rating() {
+    const [rating, setRating] = useState(0);
+
+    const handleStarPress = (newRating) => {
+        // Update the rating when a star is pressed.
+        setRating(newRating);
+    };
+
+    const renderStars = (maxStars) => {
+        const stars = [];
+        for (let i = 1; i <= maxStars; i++) {
+            stars.push(
+                <TouchableOpacity
+                    key={i}
+                    onPress={() => handleStarPress(i)}
+                >
+                    <Image
+                        source={i <= rating ? require('../assets/filledStar.png') : require('../assets/emptyStar.png')}
+                        style={{ width: 30, height: 30 }}
+                    />
+                </TouchableOpacity>
+            );
+        }
+        return stars;
+    }
+
     return (
-    <View style={{flex: 1}}>
-  <ImageBackground source={UNT1} style={{
-    flex: 1,
-    width: '104%',
-    height: '100%',
-    justifyContent: 'center',
-    resizeMode: 'contain',
-    opacity: 0.3,
-    padding: 10,
-}}>
-    <View>
-        {/* Your content goes here */}
-    </View>
-</ImageBackground>
+        <View style={{ flex: 1 }}>
+            <ImageBackground
+                source={UNT1}
+                style={{
+                    flex: 1,
+                    width: '100%',
+                    height: '100%',
+                    justifyContent: 'center',
+                    resizeMode: 'contain',
+                    opacity: 0.3,
+                    padding: 10,
+                }}
+            >
+                <View>
+                    {/* Your content goes here */}
+                </View>
+            </ImageBackground>
 
+            <Text style={{
+                fontSize: 20,
+                fontWeight: 'bold',
+                paddingTop: 15,
+                paddingBottom: 8
+            }}>Rate Us</Text>
 
-  <Text style= {{
-          fontSize: 20,
-          fontWeight: 'bold',
-          paddingTop: 15,
-          paddingBottom: 8
-        }}> Rate Us</Text>
-
-  <Text style= {{}}>Rating Us</Text>
-</View>
-
+            <View style={{ flexDirection: 'row' }}>
+                {renderStars(5)} {/* You can adjust the number of stars here */}
+            </View>
+        </View>
     );
-  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});

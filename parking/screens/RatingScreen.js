9a9d416262311d6a821c4bc873f4af0faa029618
Nathan <97767955+nathanntk@users.noreply.github.com>
2023-11-09 15:31:25 -0600

    fixed text component error ratings
    RatingsScreen.js that threw text component error is fixed 11/9 3:28pm

--- parking/screens/RatingScreen.js
+++ parking/screens/RatingScreen.js
@@ -1,63 +1,55 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';   // fixed text component error : 11/9 3:28 pm
 import { View, Text, StyleSheet, ImageBackground, Image, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import UNT1 from '../assets/UNTbackground1.png';
+import UNT1 from '../assets/UNTbackground1.png'
 
-export default function Rating() {
-    const [rating, setRating] = useState(0);
+export default class InitialPage extends Component {
+    // initialize component state with constructor
+    constructor(props) {
+        super(props);
+        this.state = {
+            rating: 0,  // initialize rating
+        };
+    }
 
-    const handleStarPress = (newRating) => {
-        // Update the rating when a star is pressed.
-        setRating(newRating);
+    // arrow methods allow for automatic binding of current instance 
+
+    handleStarPress = (newRating) => {
+        this.setState({ rating: newRating });   // newRating = star selected by user
     };
 
-    const renderStars = (maxStars) => {
+    // renderStars dynamically generates a row of stars based on the provided maxStars value
+    renderStars = (maxStars) => {
         const stars = [];
         for (let i = 1; i <= maxStars; i++) {
             stars.push(
                 <TouchableOpacity
                     key={i}
-                    onPress={() => handleStarPress(i)}
+                    onPress={() => this.handleStarPress(i)}
                 >
                     <Image
-                        source={i <= rating ? require('../assets/filledStar.png') : require('../assets/emptyStar.png')}
+                        source={i <= this.state.rating ? require('../assets/filledStar.png') : require('../assets/emptyStar.png')}
                         style={{ width: 30, height: 30 }}
                     />
                 </TouchableOpacity>
             );
         }
         return stars;
-    }
+    };
 
-    return (
-        <View style={{ flex: 1 }}>
-            <ImageBackground
-                source={UNT1}
-                style={{
-                    flex: 1,
-                    width: '100%',
-                    height: '100%',
-                    justifyContent: 'center',
-                    resizeMode: 'contain',
-                    opacity: 0.3,
-                    padding: 10,
-                }}
-            >
-                <View>
-                    {/* Your content goes here */}
-                </View>
-            </ImageBackground>
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
 
-            <Text style={{
-                fontSize: 20,
-                fontWeight: 'bold',
-                paddingTop: 15,
-                paddingBottom: 8
-            }}>Rate Us</Text>
-
-            <View style={{ flexDirection: 'row' }}>
-                {renderStars(5)} {/* You can adjust the number of stars here */}
             </View>
-        </View>
-    );
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});

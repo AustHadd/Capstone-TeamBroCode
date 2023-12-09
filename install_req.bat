6471bf267cc291259d9ae4d27219d5042c588b81
AustHadd <theaustinhaddock@gmail.com>
2023-12-08 18:13:13 -0600

    should be the last of remaining merge conflicts install_req.bat and requirements.txt

--- install_req.bat
+++ install_req.bat
@@ -1,18 +1,8 @@
 @echo off
 
 :start
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 py -m pip install -r requirements.txt
-=======
-python -m pip install -r requirements.txt
->>>>>>> e79e2632facd0b3827fa6641fd09c0b065332fa1
-
 pause
-exit
-=======
-python -m pip install -r requirements.txt
 
-pause
 exit
->>>>>>> adc081126b2cfb24613cc7609d6b1f9318ba26b5
